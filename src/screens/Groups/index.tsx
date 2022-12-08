a2eddb648cf0f55eaa42ca2a2fe8737e038e3acd
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-08 17:30:04 -0300

    Basic Code

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+const Groups: React.FC = () => {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+};
+
+export default Groups;
