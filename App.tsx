5485f5ed2de684a47f0d0fdcd03fe800f92b6e6c
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-09 09:25:20 -0300

    theme added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import Groups from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import theme from "@theme/index";
 
 const App: React.FC = () => {
-  return <Groups />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Groups />
+    </ThemeProvider>
+  );
 };
 
 export default App;
