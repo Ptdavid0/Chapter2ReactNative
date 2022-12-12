bcce80b99f01c13bcae5d4ce53e8465866de3eff
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-12 12:05:46 -0300

    Second screen completed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native";
 
 import { Container } from "./styles";
 
 const Input: React.FC<TextInputProps> = ({ ...rest }) => {
-  return <Container {...rest}></Container>;
+  const { COLORS } = useTheme();
+  return (
+    <Container {...rest} placeholderTextColor={COLORS.GRAY_300}></Container>
+  );
 };
 
 export default Input;
